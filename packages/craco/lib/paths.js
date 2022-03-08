436fe797f61d98c8c0364068148e8a401ba51957
沈关林 <shengl@xkool.org>
2022-03-08 17:00:24 +0800

    feat: Add app paths

--- packages/craco/lib/paths.js
+++ packages/craco/lib/paths.js
@@ -2,9 +2,19 @@ const fs = require("fs");
 const { log } = require("./logger");
 
 const projectRoot = fs.realpathSync(process.cwd());
+let paths = {};
+try {
+    const app = JSON.parse(process.env.APP);
+    if (app) {
+        paths = app.paths || paths;
+    }
+} catch (err) {
+    log.error(err);
+}
 
 log("Project root path resolved to: ", projectRoot);
 
 module.exports = {
-    projectRoot
+    projectRoot,
+    ...paths
 };
